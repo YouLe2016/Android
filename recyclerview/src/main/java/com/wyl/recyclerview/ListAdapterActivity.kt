591ae20381a28b5069b98ccee6aa09ac24298347
江心才子 <270628297@qq.com>
2019-10-22 10:30:50 +0800

    ListAdapter的用法

--- recyclerview/src/main/java/com/wyl/recyclerview/ListAdapterActivity.kt
+++ recyclerview/src/main/java/com/wyl/recyclerview/ListAdapterActivity.kt
@@ -1,21 +1,24 @@
 package com.wyl.recyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import com.wyl.recyclerview.databinding.ItemListAdapterBinding
 import io.ditclear.bindingadapter.BindingViewHolder
-import io.ditclear.bindingadapter.SingleTypeAdapter
 import kotlinx.android.synthetic.main.activity_list_adapter.*
-import kotlinx.android.synthetic.main.activity_list_adapter.recyclerView
-import kotlinx.android.synthetic.main.recyclerview_activity.*
 
+/**
+ * ListAdapter的用法
+ */
 class ListAdapterActivity : AppCompatActivity() {
+    private var data = List(10) {
+        User(it, "Name$it", 90f)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,46 +26,61 @@ class ListAdapterActivity : AppCompatActivity() {
 
         recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
-            adapter = UserAdapter
+            adapter = userAdapter
             addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
         }
 
-        UserAdapter.submitList(List(10) {
-            User(it, "Name$it", 90f)
-        })
+        userAdapter.submitList(data)
+
+        btRefresh.setOnClickListener {
+            val list = List(10) {
+                User(it, "Name$it", 90f)
+            }
+            userAdapter.submitList(list)
+        }
+
+        btLoadMore.setOnClickListener {
+            val list = List(10) {
+                val i = it + data.size
+                User(i, "Name$i", 80.0f)
+            }
+            val newList = data + list
+            data = newList
+            userAdapter.submitList(newList)
+        }
 
     }
-}
 
-val DiffUtil_ItemCallback = object : DiffUtil.ItemCallback<User>() {
-    override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
-        return oldItem.id == newItem.id
+
+    val diffUtilCallback = object : DiffUtil.ItemCallback<User>() {
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem == newItem
+        }
+
     }
 
-    override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
-        return oldItem == newItem
+    private val userAdapter = object :
+        ListAdapter<User, BindingViewHolder<ItemListAdapterBinding>>(diffUtilCallback) {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): BindingViewHolder<ItemListAdapterBinding> {
+            val binding =
+                ItemListAdapterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return BindingViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(
+            holder: BindingViewHolder<ItemListAdapterBinding>,
+            position: Int
+        ) {
+            holder.binding.user = getItem(position)
+        }
     }
-
-}
-
-val UserAdapter = object :
-    ListAdapter<User, BindingViewHolder<ItemListAdapterBinding>>(DiffUtil_ItemCallback) {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): BindingViewHolder<ItemListAdapterBinding> {
-        val binding =
-            ItemListAdapterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return BindingViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(
-        holder: BindingViewHolder<ItemListAdapterBinding>,
-        position: Int
-    ) {
-        holder.binding.user = getItem(position)
-    }
-
 }
 
 data class User(var id: Int, var name: String, var score: Float)
